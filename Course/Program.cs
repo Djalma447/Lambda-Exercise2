7d604a1f9a955af5339fd0fbe9c2c6fc245b8ad3
Djalma <djalma.ant@gmail.com>
2020-05-07 02:31:58 -0300

    Problem Answer

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using Course.Entities;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Globalization;
 
 namespace Course
 {
@@ -6,7 +11,27 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                List<Product> list = new List<Product>();
+                while (!sr.EndOfStream)
+                {
+                    string[] values = sr.ReadLine().Split(',');
+                    string name = values[0];
+                    double price = double.Parse(values[1], CultureInfo.InvariantCulture);
+                    list.Add(new Product(name, price));
+                }
+                var average = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+                Console.WriteLine("Average price: " + average.ToString("F2", CultureInfo.InvariantCulture));
+                var names = list.Where(p => p.Price < average).OrderByDescending(p => p.Name).Select(p => p.Name);
+                foreach(string s in names)
+                {
+                    Console.WriteLine(s);
+                }
+            }
         }
     }
 }
